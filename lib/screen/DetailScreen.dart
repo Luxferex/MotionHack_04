37ce7a387902e81b57a216e1461a30ddcfdc706d
dapraws <darrelprawira26@gmail.com>
2024-03-26 22:04:50 +0700

    Update DetailScreen

--- lib/screen/DetailScreen.dart
+++ lib/screen/DetailScreen.dart
@@ -2,6 +2,9 @@ import 'package:colorful_safe_area/colorful_safe_area.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:motion_hack_1/widgets/detailscreen/ItemViewHorizontal.dart';
+import 'package:motion_hack_1/widgets/detailscreen/ProductDetail.dart';
+import 'package:motion_hack_1/widgets/detailscreen/TokoView.dart';
 
 class DetailScreen extends StatefulWidget {
   const DetailScreen({super.key});
@@ -47,198 +50,32 @@ class _DetailScreenState extends State<DetailScreen> {
           ],
         ),
         body: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              // Product image
-              Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: Image.asset("assets/images/test123.png"),
-              ),
-              SizedBox(height: 10),
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                // Product Detail
+                ProductDetail(),
 
-              // Product name
-              Container(
-                margin: EdgeInsets.only(right: 50),
-                child: Text(
-                  "Reusable Cotton Pad Kapas Daur Ulang",
+                Divider(height: 50),
+                // Toko
+                TokoView(),
+
+                Divider(height: 50),
+
+                // Produk lainnya
+                Text(
+                  "Lainnya di toko ini",
                   style: TextStyle(
-                    fontSize: 27,
-                    fontWeight: FontWeight.w400,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
                   ),
                 ),
-              ),
-              SizedBox(height: 10),
-
-              // Product price
-              Text(
-                "Rp. 10.500",
-                style: TextStyle(
-                  color: Colors.red,
-                  fontSize: 20,
-                  fontWeight: FontWeight.w600,
-                ),
-              ),
-              SizedBox(height: 10),
-
-              // Product rating and reviews
-              Row(
-                children: [
-                  // rating
-                  Icon(
-                    Icons.star,
-                    size: 20,
-                    color: Colors.amber,
-                  ),
-                  SizedBox(width: 3),
-                  Text("4.5"),
-                  SizedBox(width: 20),
-                  // reviews
-                  Text("863 Reviews")
-                ],
-              ),
-              SizedBox(height: 10),
-
-              // Product description
-              Text(
-                "Deskripsi Produk",
-                style: TextStyle(fontSize: 20),
-              ),
-              SizedBox(height: 10),
-              Text(
-                "Reusable Cotton Pads terbuat dari serat bambu yang SANGAT LEMBUT dengan daya serap tinggi dan sangat breathable. Produk ini digunakan sebagai pengganti kapas wajah sekali pakai dan dapat digunakan berulang kali hingga kurang lebih 1000x pemakaian. \n\nMaterial : Bamboo Cotton (70% Bamboo+ 30% cotton)\nLayer Size : 8cm\nWarna : Putih\n\nPetunjuk perawatan : Setelah penggunaan, cuci dengan detergen lembut dan air hangat untuk menjaga tekstur dan daya serap pads. Hindari cahaya matahari langsung saat menjemur karena suhu yang terlalu tinggi dapat mengakibatkan kain menyusut atau berubah bentuk (suhu maksimum 30-40 Celcius)",
-              ),
-
-              // Toko
-              Container(
-                  child: Column(
-                children: [
-                  // Profile Toko
-                  Row(
-                    children: [
-                      ClipRRect(
-                        borderRadius: BorderRadius.circular(72),
-                        child: Container(
-                          width: 90,
-                          height: 90,
-                          child: Image.asset(
-                            "assets/images/test123.png",
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                      ),
-                      SizedBox(width: 30),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text("Vegetalami"),
-                          Text("Online 2 jam lalu"),
-                          Row(
-                            children: [
-                              Icon(
-                                Icons.location_pin,
-                                size: 15,
-                              ),
-                              // Image.asset('assets/icons/ic_location.png'),
-                              SizedBox(width: 2),
-                              Text("Bandung")
-                            ],
-                          ),
-                        ],
-                      )
-                    ],
-                  ),
-                  // Rating & Ikuti Button
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      // Rating
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Row(
-                            children: [
-                              Icon(
-                                Icons.star,
-                                size: 20,
-                                color: Colors.amber,
-                              ),
-                              SizedBox(width: 7),
-                              Text(
-                                "4.5",
-                                style: TextStyle(
-                                  fontSize: 15,
-                                  fontWeight: FontWeight.w300,
-                                ),
-                              ),
-                              SizedBox(width: 7),
-                              Text(
-                                "Rata-rata ulasan",
-                                style: TextStyle(
-                                  fontSize: 15,
-                                  fontWeight: FontWeight.w300,
-                                ),
-                              ),
-                            ],
-                          ),
-                          Row(
-                            children: [
-                              Icon(
-                                Icons.access_time,
-                                size: 20,
-                                color: Colors.grey,
-                              ),
-                              SizedBox(width: 7),
-                              Text(
-                                "± 1 jam",
-                                style: TextStyle(
-                                  fontSize: 15,
-                                  fontWeight: FontWeight.w300,
-                                ),
-                              ),
-                              SizedBox(width: 7),
-                              Text(
-                                "Pesanan diproses",
-                                style: TextStyle(
-                                  fontSize: 15,
-                                  fontWeight: FontWeight.w300,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ],
-                      ),
-                      // Ikuti Button
-                      Container(
-                        padding: EdgeInsets.symmetric(horizontal: 40),
-                        decoration: BoxDecoration(
-                          border: Border.all(
-                            width: 3,
-                            color: Color(0xFF436850),
-                          ),
-                          borderRadius: BorderRadius.circular(15),
-                        ),
-                        child: TextButton(
-                          onPressed: () {},
-                          child: Text(
-                            "Ikuti",
-                            style: TextStyle(
-                              color: Color(0xFF436850),
-                              fontSize: 20,
-                              fontWeight: FontWeight.w500,
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
-                  )
-                ],
-              ))
-
-              // Produk lainnya
-            ],
+                SizedBox(height: 20),
+                ItemViewHorizontal(),
+              ],
+            ),
           ),
         ),
         bottomNavigationBar: Padding(
