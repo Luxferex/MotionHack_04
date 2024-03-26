3273861a2ffbc91547dc1afcd125af2c7e40377e
dapraws <darrelprawira26@gmail.com>
2024-03-26 21:31:15 +0700

    Add Detail Screen

---
+++ lib/screen/DetailScreen.dart
@@ -0,0 +1,18 @@
+import 'package:colorful_safe_area/colorful_safe_area.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class DetailScreen extends StatefulWidget {
+  const DetailScreen({super.key});
+
+  @override
+  State<DetailScreen> createState() => _DetailScreenState();
+}
+
+class _DetailScreenState extends State<DetailScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
